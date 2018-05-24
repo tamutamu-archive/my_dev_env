f0aa96927c3c3bda78ab2feb46bbf83886a2d893
tamutamu <tamu.0.0.tamu@gmail.com>
2018-05-24 15:42:13 +0900

    Add script of installing eclipse css.

---
+++ eclipse/color-theme/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eu
+
+mv /home/tamutamu/dev/eclipse/plugins/org.eclipse.ui.themes_1.2.1.v20170809-1435/css/e4_default_gtk{.css,.css.bk}
+
+cp ./e4_default_gtk.css /home/tamutamu/dev/eclipse/plugins/org.eclipse.ui.themes_1.2.1.v20170809-1435/css/
