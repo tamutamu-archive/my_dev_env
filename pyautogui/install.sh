5de4bf7d28f148dbd29d560e7ca23f96b08d7168
tamutamu <tamu.0.0.tamu@gmail.com>
2018-05-30 15:36:50 +0900

    Add pyautogui scripts.

--- pyautogui/install.sh
+++ pyautogui/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -eu
 
+CURDIR=$(cd $(dirname $0); pwd)
+
 sudo apt -y install scrot
 
 pip install Xlib
 pip install pyautogui
 pip install pynput
 pip install opencv-python
+
+
+## Install my scripts.
+mkdir /opt/scripts/pyautogui
+ln -s ${CURDIR}/scripts /opt/scripts/pyautogui/
--- pyautogui/scripts/eclipse/select_editor.py
+++ pyautogui/scripts/eclipse/select_editor.py
@@ -4,13 +4,12 @@ import pyautogui as pg
 
 s = pg.screenshot()
 zentai = cv2.cvtColor(np.array(s),cv2.COLOR_BGR2GRAY)
-target = cv2.imread('/home/tamutamu/target.png',cv2.IMREAD_GRAYSCALE)
+target = cv2.imread('./eclipse1.png',cv2.IMREAD_GRAYSCALE)
 
 res = cv2.matchTemplate(zentai, target, cv2.TM_CCOEFF_NORMED)
 
 min_val,max_bal,min_loc,top_left = cv2.minMaxLoc(res)
-cll = (top_left[0] + 52, top_left[1] + 42)
-
-print(cll)
+cll = (top_left[0], top_left[1] + 153)
 
 pg.click(cll)
+
