a13bc84be39ee340fec12459b86aa63ffa986d00
tamutamu <tamu.0.0.tamu@gmail.com>
2018-06-26 13:29:49 +0900

    Fix terminator install script.

--- terminator/install.sh
+++ terminator/install.sh
@@ -10,3 +10,11 @@ pushd ${CURDIR}
 ### Install terminator
 sudo apt -y install python-notify terminator
 
+mkdir -p ~/.local/bin/
+
+cat << EOT > ~/.local/bin/terminator.sh
+#!/bin/bash
+terminator --geometry 1200x700+240+125
+EOT
+
+chmod +x ~/.local/bin/terminator.sh
