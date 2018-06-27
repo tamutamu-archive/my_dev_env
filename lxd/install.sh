6c515549d12450d8c187bf04c0c0ccad363a42ea
tamutamu <tamu.0.0.tamu@gmail.com>
2018-06-27 16:38:24 +0900

    Fix lxd install script.
    Add proxy setting.

--- lxd/install.sh
+++ lxd/install.sh
@@ -15,7 +15,18 @@ cat _conf/init.yml | sudo lxd init --preseed
 ### Create machine dir.
 sudo mkdir -p /var/lxd-machine/
 sudo cp -r ${CURDIR}/_conf /var/snap/lxd/common/
+
+# Setting dnsmasq conf.
 sudo lxc network set lxdbr0 raw.dnsmasq 'addn-hosts=/var/snap/lxd/common/_conf/lxd_hosts'
+
+# Setting proxy.
+set +u
+if [ ! -z "${http_proxy}" ];then
+  sudo lxc config set core.proxy_http http://tkyproxy.intra.tis.co.jp:8080
+  sudo lxc config set core.proxy_https http://tkyproxy.intra.tis.co.jp:8080
+  sudo lxc config set core.proxy_ignore_hosts localhost
+fi
+set -u
 
 sudo cp -r ${CURDIR}/_mng /var/snap/lxd/common/
 sudo cp -r ${CURDIR}/_common /var/snap/lxd/common/
