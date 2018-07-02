12f467d63281f29aa1806b6dc83d5e2a3e1a1639
tamutamu <tamu.0.0.tamu@gmail.com>
2018-07-02 19:05:58 +0900

    Fix installing lxd.

--- lxd/install.sh
+++ lxd/install.sh
@@ -54,6 +54,7 @@ fs.inotify.max_queued_events = 1048576
 fs.inotify.max_user_instances = 1048576
 fs.inotify.max_user_watches = 1048576
 vm.max_map_count = 262144
+kernel.dmesg_restrict = 1
 EOT
 
 
