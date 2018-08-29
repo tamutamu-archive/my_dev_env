043791ff06ebd6493ccbb3abd0f24a37aa69d195
tamutamu <tamu.0.0.tamu@gmail.com>
2018-08-29 16:11:05 +0900

    Update and fix install rofi.

--- rofi/install.sh
+++ rofi/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+CURDIR=$(cd $(dirname $0); pwd)
+
 sudo add-apt-repository ppa:jasonpleau/rofi
 sudo apt -y update
 sudo apt -y install rofi
+
+ln -s ${CURDIR}/config/rofi ~/.config/rofi
