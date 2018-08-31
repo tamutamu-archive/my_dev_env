7f0b8fed519a7a7a4d995cc0014eb6d70f27b5ad
tamutamu <tamu.0.0.tamu@gmail.com>
2018-08-31 13:48:40 +0900

    Fix ranger install script.

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-sudo apt -y install ranger
+
+TMPDIR=$(mktemp -d --suffix=.mydev_tmp)
+trap "sudo rm -rf ${TMPDIR}" EXIT
+
+pushd ${TMPDIR}
+
+git clone --depth 1 https://github.com/ranger/ranger.git
+cd ranger
+sudo make install
+
+
+popd
