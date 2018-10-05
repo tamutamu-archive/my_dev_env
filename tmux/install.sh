ba577aeb3e6efbf867a2217861657c57b840c237
tamutamu <tamu.0.0.tamu@gmail.com>
2018-10-05 09:29:13 +0900

    Update tmux.

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,7 +6,16 @@ cd ${CURDIR}
 
 
 
-### Install tmux
-sudo apt -y install tmux
+### Install tmux.
+sudo apt update 
+sudo apt install -y build-essential automake libevent-dev ncurses-dev
+mkdir -p /tmp/src
+cd /tmp/src && git clone https://github.com/tmux/tmux.git
+cd /tmp/src/tmux && sh autogen.sh && ./configure && make 
+cp /tmp/src/tmux/tmux ~/.local/bin/
+echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 
+
+### Copy my config.
 ln -s ${CURDIR}/../.dotfiles/.tmux.conf ~/.tmux.conf
